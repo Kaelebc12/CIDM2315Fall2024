835e05fd3201a35945e436a49babd83f0efc27a2
Kaelebc12 <kmcedillo1@buffs.wtamu.edu>
2024-09-02 17:33:09 -0500

    Add files via upload

---
+++ homework1.cs
@@ -0,0 +1,13 @@
+﻿namespace Homework1;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+       double x = 2.5;
+       double y = 3.3;
+       double z = 4 * x * x + 3 * y;
+       Console.WriteLine("x = 2.5, y = 3.3");
+        Console.WriteLine("Value of z: " + z);
+    }
+}
