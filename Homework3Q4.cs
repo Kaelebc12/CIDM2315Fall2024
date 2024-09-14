07da88b22598d6bd5093d05a6d94d7728d09af0e
Kaelebc12 <kmcedillo1@buffs.wtamu.edu>
2024-09-14 15:54:42 -0500

    Upload Homework3 Q4

---
+++ Homework3Q4.cs
@@ -0,0 +1,23 @@
+﻿namespace Homework3Q4;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+                // Input the integer N
+        Console.Write("Assign an in to N: ");
+        int N = int.Parse(Console.ReadLine());
+
+        // Print the square pattern
+        for (int i = 1; i <= N; i++)  // Loop for each row
+        {
+            for (int j = 1; j <= i; j++)  // Loop for each column in the current row
+            {
+                Console.Write(i);  // Print the row number
+            }
+            Console.WriteLine();  // Move to the next line after printing all columns in the current row
+        }
+
+
+    }
+}
