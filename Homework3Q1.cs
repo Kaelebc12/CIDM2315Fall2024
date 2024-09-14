3528aa441f842fc1c2d734609606dec000a62212
Kaelebc12 <kmcedillo1@buffs.wtamu.edu>
2024-09-14 15:29:32 -0500

    upload homework 3 Q1

---
+++ Homework3Q1.cs
@@ -0,0 +1,42 @@
+﻿namespace Homework3;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Prompt the user to enter a number
+        Console.Write("Enter a number: ");
+        int N = Convert.ToInt32(Console.ReadLine());
+
+        // Check if the number is less than 2
+        if (N < 2)
+        {
+            Console.WriteLine($"{N} is non-prime");
+            return;
+        }
+
+        // Assume the number is prime
+        bool isPrime = true;
+
+        // Check for factors from 2 to N-1
+        for (int i = 2; i < N; i++)
+        {
+            if (N % i == 0)
+            {
+                isPrime = false;
+                break;
+            }
+        }
+
+        // Output result based on the prime check
+        if (isPrime)
+        {
+            Console.WriteLine($"{N} is prime");
+        }
+        else
+        {
+            Console.WriteLine($"{N} is non-prime");
+        }
+    }
+    }
+
