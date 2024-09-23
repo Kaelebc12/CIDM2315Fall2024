f74d510ee14fd7350cf97187fcdfee29f0716209
Kaelebc12 <kmcedillo1@buffs.wtamu.edu>
2024-09-23 11:43:38 -0500

    upload Homework4Q2

---
+++ Homework4Q2.cs
@@ -0,0 +1,47 @@
+﻿namespace Homework4Q2;
+
+class Program
+{
+    
+    static void Input(){
+    
+        Console.Write("N is: ");
+        int a = int.Parse(Console.ReadLine());
+        Console.Write("Shape is ");
+        string b = Console.ReadLine();
+        
+        if(a == a && b == "left"){
+            for(int i = 0;  i < a; i++){
+            for(int j = 0; j < a; j++){
+                if(i >= j)
+                    Console.Write("*");
+            }
+                Console.WriteLine("");
+        }
+    }
+        else if(a == a && b == "right")
+    {
+        for(int i = 0;  i <= a; i++){
+            for(int j = 0; j <= a - i; j++)
+            {
+                Console.Write(" ");
+            }   
+            for(int k = 1; k <=i; k++)
+            {
+                Console.Write("*");
+            }
+            Console.WriteLine("");
+            }   
+        }    
+        else
+        {
+            Console.WriteLine("Please enter an integer and side.");
+        }
+}
+
+    static void Main(string[] args)
+    {
+        Input();
+    }
+}
+
