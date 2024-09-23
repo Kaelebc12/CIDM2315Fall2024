63e13117bca1f4a5ab43086dfbf654db21f6b66c
Kaelebc12 <kmcedillo1@buffs.wtamu.edu>
2024-09-23 10:51:57 -0500

    homework4Q1

---
+++ Homework4.cs
@@ -0,0 +1,20 @@
+﻿namespace Homework4;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+    Console.Write("A = ");
+    int a = int.Parse(Console.ReadLine());
+    Console.Write("B = ");
+    int b = int.Parse(Console.ReadLine());
+
+    if (a > b)
+    {
+        Console.WriteLine("The largest number is " + a);
+    }
+    else {
+        Console.WriteLine("The largest number is: " + b);
+    }
+    }
+}
