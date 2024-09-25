812733a613d62b0f512598fd8155ad0c1c276df5
Kaelebc12 <kmcedillo1@buffs.wtamu.edu>
2024-09-25 15:57:26 -0500

    upload homework 5Q3

---
+++ Homework5Q3.cs
@@ -0,0 +1,53 @@
+﻿namespace Homework5Q3;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+          createAccount();
+    }
+
+    static bool checkAge(int birthYear)
+    {
+        int currentYear = DateTime.Now.Year;
+        int age = currentYear - birthYear;
+        return age >= 18;
+    }
+
+    static void createAccount()
+    {
+        Console.Write("Enter username: ");
+        string username = Console.ReadLine();
+
+        Console.Write("Enter password: ");
+        string password = Console.ReadLine();
+
+        Console.Write("Re-enter password: ");
+        string confirmPassword = Console.ReadLine();
+
+        Console.Write("Enter birth year: ");
+        int birthYear;
+
+        
+        while (!int.TryParse(Console.ReadLine(), out birthYear) || birthYear > DateTime.Now.Year)
+        {
+            Console.Write("Invalid input. Please enter a valid birth year: ");
+        }
+
+        if (checkAge(birthYear))
+        {
+            if (password == confirmPassword)
+            {
+                Console.WriteLine("Account is created successfully");
+            }
+            else
+            {
+                Console.WriteLine("Wrong password");
+            }
+        }
+        else
+        {
+            Console.WriteLine("Could not create an account");
+        }
+    }
+}
