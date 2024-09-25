fc44a50b89f709d07feb23a8119aba494df908fc
Kaelebc12 <kmcedillo1@buffs.wtamu.edu>
2024-09-25 15:44:53 -0500

    upload homework 5Q1

---
+++ Homework5Q1.cs
@@ -0,0 +1,22 @@
+﻿namespace Homework5;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int largest = GetLargestNumber();
+        Console.WriteLine($"The largest number is: {largest}");
+    }
+
+    static int GetLargestNumber()
+    {
+        Console.Write("Enter the first integer: ");
+        int firstNumber = Convert.ToInt32(Console.ReadLine());
+
+        Console.Write("Enter the second integer: ");
+        int secondNumber = Convert.ToInt32(Console.ReadLine());
+
+        return (firstNumber > secondNumber) ? firstNumber : secondNumber;
+    }
+    }
+
