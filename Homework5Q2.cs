6ce92e9addd78a7d414b03d85ffde401e3cb8af7
Kaelebc12 <kmcedillo1@buffs.wtamu.edu>
2024-09-25 15:57:00 -0500

    upload homework 5Q2

---
+++ Homework5Q2.cs
@@ -0,0 +1,36 @@
+﻿namespace Homework5Q2;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int largest = GetLargestOfFour();
+        Console.WriteLine($"The largest number is: {largest}");
+    }
+
+    static int GetLargestOfFour()
+    {
+        Console.Write("Enter the first integer: ");
+        int firstNumber = Convert.ToInt32(Console.ReadLine());
+
+        Console.Write("Enter the second integer: ");
+        int secondNumber = Convert.ToInt32(Console.ReadLine());
+
+        Console.Write("Enter the third integer: ");
+        int thirdNumber = Convert.ToInt32(Console.ReadLine());
+
+        Console.Write("Enter the fourth integer: ");
+        int fourthNumber = Convert.ToInt32(Console.ReadLine());
+
+        int largestFirstPair = GetLargestNumber(firstNumber, secondNumber);
+        int largestSecondPair = GetLargestNumber(thirdNumber, fourthNumber);
+
+        return GetLargestNumber(largestFirstPair, largestSecondPair);
+    }
+
+    static int GetLargestNumber(int a, int b)
+    {
+        return (a > b) ? a : b;
+    }
+    }
+
